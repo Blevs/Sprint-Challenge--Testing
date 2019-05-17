04e86c0536a4ab92245436e3e99bef6f9b59f645
Henry Blevins <heblevi@gmail.com>
2019-05-17 11:10:00 -0400

    Configure project and database

---
+++ data/migrations/20190517110737_bootstrap.js
@@ -0,0 +1,11 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('games', tbl => {
+    tbl.increments();
+    tbl.string('title').notNullable();
+    tbl.integer('releaseYear');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('games');
+};
