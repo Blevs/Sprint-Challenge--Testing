143389b98ead6ff87cea923684502719554186ab
Henry Blevins <heblevi@gmail.com>
2019-05-17 11:33:50 -0400

    Write post routes

--- games/routes.js
+++ games/routes.js
@@ -1,5 +1,17 @@
 const express = require('express');
+const db = require('../data/dbConfig.js');
 
 const router = express.Router();
+
+router.post('/', (req, res) => {
+  const game = req.body;
+  if (game && game.title && game.genre) {
+    db('games').insert(game).then(([id]) => db('games').where({id}).first())
+      .then(game => res.status(201).json(game))
+      .catch(() => res.status(500).json({message: 'Error creating game'}));
+  } else {
+    res.status(422).json({message: 'Requires title and genre'});
+  }
+});
 
 module.exports = router;
